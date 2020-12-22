c03a4eedbfb21f6cc001ccf3f8223b097233c62b
castic96 <zdenek.castoral.jr@gmail.com>
2020-12-22 12:17:39 +0100

    Úprava GUI - vytvoření akcí pro ukončení a přepnutí typu běhu.

--- src/main/java/controller/SimulationWindowController.java
+++ src/main/java/controller/SimulationWindowController.java
@@ -1,4 +1,102 @@
 package controller;
 
-public class SimulationWindowController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.enums.LaunchType;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class SimulationWindowController implements Initializable {
+
+    @FXML
+    private ComboBox<String> comboBox;
+
+    @FXML
+    private Label labelStepByStep;
+
+    @FXML
+    private Label labelRunByTime;
+
+    @FXML
+    private Label labelStepByStepCurrTime;
+
+    @FXML
+    private Label labelRunByTimeCurrTime;
+
+    @FXML
+    private Label labelRunByTimeMaxTime;
+
+    @FXML
+    private Button buttonStep;
+
+    @FXML
+    private Button buttonStart;
+
+    @FXML
+    private Button buttonStop;
+
+    @FXML
+    private TextField textFieldMaxTime;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboBox.getItems().addAll(LaunchType.STEP_BY_STEP.getValue(), LaunchType.RUN_BY_TIME.getValue());
+        comboBox.setEditable(false);
+        comboBox.setValue(LaunchType.STEP_BY_STEP.getValue());
+        stepByStepEnable();
+    }
+
+    @FXML
+    public void exitApplication() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss yyyy/MM/dd");
+        LocalDateTime end = LocalDateTime.now();
+        System.out.println("Application ended: "+ dtf.format(end));
+        System.exit(0);
+    }
+
+    @FXML
+    public void comboBoxChange() {
+        if (comboBox.getValue().equals(LaunchType.STEP_BY_STEP.getValue())) {
+            stepByStepEnable();
+        }
+        else {
+            runByTimeEnable();
+        }
+    }
+
+    private void stepByStepEnable() {
+        labelStepByStep.setDisable(false);
+        labelStepByStepCurrTime.setDisable(false);
+        buttonStep.setDisable(false);
+
+        labelRunByTime.setDisable(true);
+        labelRunByTimeCurrTime.setDisable(true);
+        labelRunByTimeMaxTime.setDisable(true);
+        buttonStart.setDisable(true);
+        buttonStop.setDisable(true);
+        textFieldMaxTime.setDisable(true);
+    }
+
+    private void runByTimeEnable() {
+        labelStepByStep.setDisable(true);
+        labelStepByStepCurrTime.setDisable(true);
+        buttonStep.setDisable(true);
+
+        labelRunByTime.setDisable(false);
+        labelRunByTimeCurrTime.setDisable(false);
+        labelRunByTimeMaxTime.setDisable(false);
+        buttonStart.setDisable(false);
+        buttonStop.setDisable(false);
+        textFieldMaxTime.setDisable(false);
+    }
+
 }
---
+++ src/main/java/model/enums/LaunchType.java
@@ -0,0 +1,16 @@
+package model.enums;
+
+public enum LaunchType {
+    STEP_BY_STEP("Step by step"),
+    RUN_BY_TIME("Run by time");
+
+    private final String value;
+
+    LaunchType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
